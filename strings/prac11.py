2b7f45cf9adea171a16cfcc85c186a08479ef30a
sujalrathod333 <sujalrathod223@gmail.com>
2026-07-01 21:55:06 +0530

    01-july-2026

---
+++ strings/prac11.py
@@ -0,0 +1,15 @@
+def longestPefix(strs):
+    if not strs:
+        return ""
+    
+    for i in range(len(strs[0])):
+        char = strs[0][i]
+        for s in strs[1:]:
+            if i >= len(s) or s[i] != char:
+                return strs[0][:i]
+    return strs[0]
+
+
+strs = ["flower","flow","flight"]
+
+print(longestPefix(strs))
