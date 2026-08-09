e429d7c6d44134b9abca36af337d657ed55c7993
sujalrathod333 <sujalrathod223@gmail.com>
2026-08-09 16:21:50 +0530

    09-08-26

---
+++ graph/prac8.py
@@ -0,0 +1,9 @@
+#1470 shuffle the array
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans=[]
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+        return ans
+        
